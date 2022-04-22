c34e2a0b709f1622801b416006e70965cd4def79
KallNascimento <iksn7@icloud.com>
2022-04-22 08:13:12 -0300

    filter by country

--- src/views/homePage.tsx
+++ src/views/homePage.tsx
@@ -1,5 +1,7 @@
 import { useFetch } from '../hooks/useFetch';
-import React from "react";
+import "./HomePage.css";
+import React, { ChangeEvent, useEffect, useState } from "react";
+import { useParams } from 'react-router-dom'
 import Paper from '@mui/material/Paper';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -9,9 +11,18 @@ import TableHead from '@mui/material/TableHead';
 import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
 import TableSortLabel from '@mui/material/TableSortLabel/TableSortLabel';
+import TableFooter from '@mui/material/TableFooter';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import  Button  from '@mui/material/Button';
+import { IconButton } from '@mui/material';
+import { Search } from '@mui/icons-material';
 
 
 type FetchUsers = {
+    gender: string;
+    email: string;
+    nat: string
     login: {
         uuid: string
     },
@@ -19,72 +30,129 @@ type FetchUsers = {
         first: string,
         last: string
     };
-    gender: string;
-    email: string;
-    nat: string
 }
 
 function HomePage() {
-    const [page, setPage] = React.useState(0);
-    const [rowsPerPage, setRowsPerPage] = React.useState(10);
+    const pages = [10, 20, 30, 40, 50, 100, 1000]
+    const [page, setPage] = useState(0);
+    const [rowsPerPage, setRowsPerPage] = useState(pages[page]);
+    const [country, setCountry] = useState('');
+    const [filterUser, setFilterUser] = useState<FetchUsers[] | null>(null)
+
+    // const [userData, setUserData] = useState<FetchUsers[]>([]);
+
+    // useEffect(() => {
+    //     const {data} = useFetch<FetchUsers[]>(`?results=${rowsPerPage}&nat=${country}`);
+    //     if(data)setUserData(data)
+    // }, [])
 
     const handleChangePage = (event: unknown, newPage: number) => {
         setPage(newPage);
     };
 
-    const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setRowsPerPage(+event.target.value);
+    const handleChangeRowsPerPage = (event: any) => {
+        setRowsPerPage(parseInt(event.target.value, 10))
         setPage(0);
-    };
+    }
 
+    const recordsAfterPagingAndSorting = () => {
+        return filterUser?.slice(page * rowsPerPage, (page + 1) * rowsPerPage)
+    }
+
+    // const handleCountryChange = (event: any) => {
+    //     setCountry(event.target.value)
+    // }
+
+    const headerCells = [
+        { id: 'fullname', label: 'Name' },
+        { id: 'email', label: 'E-mail' },
+        { id: 'gender', label: 'Gender' },
+        { id: 'nat', label: 'Country' }
+    ]
 
     const { data: usersData, isFetching } =
-        useFetch<FetchUsers[]>(`?result=${setRowsPerPage(rowsPerPage)}`);
+        useFetch<FetchUsers[]>(`?results=${rowsPerPage}&nat=${country}`);
+
+    useEffect(() => {
+        if (usersData) {
+            setFilterUser(usersData)
+        }
+    }, [usersData])
+
+    const onTextFieldChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        console.log(e.target.value);
+        setCountry(e.target.value)
+        if (Array.isArray(filterUser) && filterUser?.length > 0) {
+            const newArray = filterUser?.filter(user => {
+                return user.nat === country
+            })
+            console.log(newArray);
+            if (newArray?.length > 0) {
+                setFilterUser(newArray)
+            }
+        }
+    }
+    const filterByCountry = () =>{
+        //    need to implement
+
+    }
+
     return (
-        <Paper sx={{ width: '100%' }}>
+        <Paper sx={{ width: '100%' }} className="HomePage">
+            <Typography variant="h5" component="h5">
+                Users Table
+            </Typography>
+           
+                <TextField
+                    onChange={e => setCountry(e.target.value)}
+                    id="search-field"
+                    label="Search by Country"
+                    variant="filled"
+                    value=""
+                />
+                <Button variant='contained' onClick={filterByCountry}>Search</Button>
+            
+
             <TableContainer sx={{ minHeight: 500 }}>
                 <Table>
                     <TableHead>
                         <TableRow>
-                            <TableCell>Name</TableCell>
-                            <TableCell>E-Mail</TableCell>
-                            <TableCell>Gender</TableCell>
-                            
-                            <TableCell>
-                                
-                                Country
-                                
+                            {isFetching && <TableCell><p>Loading data...</p></TableCell>}
+                            {headerCells.map(headCell => (
+                                <TableCell key={headCell.id}>
+                                    {headCell.label}
                                 </TableCell>
+                            ))
+                            }
                         </TableRow>
                     </TableHead>
-                    {isFetching && <>
-                        <p>Loading data...</p>
-                    </>
-                    }
-                    {usersData?.map((users) => {
-
-                        return (
-                            <TableRow key={users.login.uuid}>
-                                <TableCell>{users.name.first} {users.name.last}</TableCell>
-                                <TableCell>{users.email}</TableCell>
-                                <TableCell>{users.gender}</TableCell>
-                                <TableCell>{users.nat}</TableCell>
-                            </TableRow>
-                        )
-                    })
-                    }
+                    <TableBody>
+                        {filterUser?.map((users) => {
+                            return (
+                                <TableRow key={users.login.uuid}>
+                                    <TableCell>{users.name.first} {users.name.last}</TableCell>
+                                    <TableCell>{users.email}</TableCell>
+                                    <TableCell>{users.gender}</TableCell>
+                                    <TableCell>{users.nat}</TableCell>
+                                </TableRow>
+                            )
+                        })
+                        }
+                    </TableBody>
+                    <TableFooter>
+                        <TablePagination
+                            count={usersData?.length}
+                            rowsPerPageOptions={pages}
+                            rowsPerPage={rowsPerPage}
+                            page={page}
+                            onPageChange={handleChangePage}
+                            onRowsPerPageChange={handleChangeRowsPerPage}
+                        />
+                    </TableFooter>
                 </Table>
-             {/* <TablePagination
-             
-             rowsPerPageOptions={[10, 50, 100, { value: -1, label: 'All' }]} 
-             
-             rowsPerPage={rowsPerPage}
-             page={page}
-             onPageChange={handleChangePage}
-              onRowsPerPageChange={handleChangeRowsPerPage}
-             />*/}
-            </TableContainer> 
-        </Paper>
+            </TableContainer>
+
+        </Paper >
     )
 }
 
--- src/views/userDetail.tsx
+++ src/views/userDetail.tsx
@@ -1,41 +1,114 @@
 import { useFetch } from "../hooks/useFetch";
+import { MapContainer, TileLayer, Marker, useMap, Popup } from 'react-leaflet';
 
+import { useState } from "react";
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+
+import Typography from '@mui/material/Typography';
+
+import './userDetail.css'
+import Box from "@mui/material/Box";
+import IconButton from "@mui/material/IconButton";
+import MaleIcon from '@mui/icons-material/Male'
+import FemaleIcon from '@mui/icons-material/Female'
 export default function UserDetail() {
 
     interface User {
-        id: string,
+        login: {
+            uuid: string
+        },
         name: {
             title: string,
             first: string,
             last: string
         };
         gender: string;
+        nat: string;
         email: string;
         location: {
-            x: Number,
-            y: Number,
-            z: Number,
+            latitude: number,
+            longitude: number,
         };
         picture: {
-            thumbnail: string
+            large: string
         }
     }
 
+    const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0]);
     const { data: userData, isFetching } =
         useFetch<User[]>(``);
+
     return (
         <>
             {isFetching && <p>Loading...</p>}
 
             {userData?.map((user) => {
+                [user.location.latitude, user.location.longitude] = selectedPosition;
+                let flag = user.nat.toLowerCase()
+                const fullName = [user.name.first, " ", user.name.last]
                 return (
-                    <div>
-                        <img src={user.picture.thumbnail} alt="" srcset="" />
-                        <span key={user.email}>{user.name.title}</span>
-                        <span>{user.name.first}</span>
-                        <span>{user.gender}</span>
-                        <span>{user.email}</span>
-                    </div>
+                    <>
+
+                        <Card sx={{ display: 'flex', maxWidth: 400 }} key={user.login.uuid}>
+                            <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                                <CardContent sx={{ flex: '1 0 auto' }}>
+                                    <Typography component="div" variant="h5">
+                                        {fullName}
+                                    </Typography>
+
+                                    <Typography variant="subtitle1" color="text.secondary" component="div">
+                                        {user.email}
+                                    </Typography>
+                                </CardContent>
+                                <Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }}>
+                                    <IconButton aria-label="previous">
+                                        {user.gender === 'male' ? <MaleIcon /> : <FemaleIcon />}
+                                    </IconButton>
+                                    <CardMedia
+                                        component="img"
+                                        sx={{ width: 36, height: 26 }}
+                                        image={`https://flagcdn.com/48x36/${flag}.png`}
+                                        alt="Live from space album cover"
+                                    />
+                                </Box>
+                            </Box>
+                            <CardMedia
+                                component="img"
+                                sx={{ width: 151 }}
+                                image={user.picture.large}
+                                alt="Live from space album cover"
+                            />
+                        </Card>
+
+                        <div className="mapWrapper">
+                            <MapContainer
+                                center={selectedPosition}
+                                zoom={15}
+                                scrollWheelZoom={false}
+                                dragging={false}
+                                doubleClickZoom={false}
+                                attributionControl={false}
+                                zoomControl={false}
+                            >
+                                <TileLayer
+                                    attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                                />
+
+                                <Marker position={selectedPosition}>
+                                    <Popup>
+                                        <p>{fullName}'s House.</p>
+                                    </Popup>
+
+                                </Marker>
+                            </MapContainer>
+
+                        </div>
+
+                    </>
                 );
             })}
         </>
